de3ee1c9d977021f13807d62aa7d96a33171d79f
AndrewB <avborovets@gmail.com>
2023-12-25 02:39:44 +0300

    first tx via js code

--- frontend/src/web3/main.js
+++ frontend/src/web3/main.js
@@ -1,10 +1,31 @@
 const ethers = require('ethers');
 
 const privateKey = '0xae803ff85f84549a43946ca2e90cb9ed827048ed4029414cd0ca9c8bfbb156bb';
-const contractAddress = '0xF17A1024E19410A384E403e0c6af81c9883bDDB5';
+const publicKey = '0x3f16be0eb6abd3d6cd510299745a30f901557524040fbfde4bd9d5348c91660e79828018122dc9c7efa9fd0eeaa56fc773a1e4fb7aec10b3044e245e1cc2356d';
+const contractAddress = '0xA4cA0353229F47b0073c35b328E454F1B1fa1Ab4';
 const contractABI = [
     {
         "inputs": [
+            {
+                "internalType": "bytes32",
+                "name": "message",
+                "type": "bytes32"
+            },
+            {
+                "internalType": "uint8",
+                "name": "v",
+                "type": "uint8"
+            },
+            {
+                "internalType": "bytes32",
+                "name": "r",
+                "type": "bytes32"
+            },
+            {
+                "internalType": "bytes32",
+                "name": "s",
+                "type": "bytes32"
+            },
             {
                 "internalType": "string",
                 "name": "site",
@@ -16,12 +37,12 @@ const contractABI = [
                 "type": "string"
             },
             {
-                "internalType": "string",
+                "internalType": "bytes",
                 "name": "_password",
-                "type": "string"
+                "type": "bytes"
             }
         ],
-        "name": "addSite",
+        "name": "addUserToSite",
         "outputs": [],
         "stateMutability": "nonpayable",
         "type": "function"
@@ -30,6 +51,275 @@ const contractABI = [
         "inputs": [],
         "stateMutability": "nonpayable",
         "type": "constructor"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": false,
+                "internalType": "string",
+                "name": "site",
+                "type": "string"
+            },
+            {
+                "indexed": false,
+                "internalType": "string",
+                "name": "login",
+                "type": "string"
+            },
+            {
+                "indexed": false,
+                "internalType": "bytes",
+                "name": "password",
+                "type": "bytes"
+            }
+        ],
+        "name": "NewUser",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": false,
+                "internalType": "string",
+                "name": "site",
+                "type": "string"
+            },
+            {
+                "indexed": false,
+                "internalType": "string",
+                "name": "oldLogin",
+                "type": "string"
+            },
+            {
+                "indexed": false,
+                "internalType": "string",
+                "name": "newLogin",
+                "type": "string"
+            }
+        ],
+        "name": "changeLogin",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": false,
+                "internalType": "string",
+                "name": "site",
+                "type": "string"
+            },
+            {
+                "indexed": false,
+                "internalType": "string",
+                "name": "login",
+                "type": "string"
+            },
+            {
+                "indexed": false,
+                "internalType": "bytes",
+                "name": "newPassword",
+                "type": "bytes"
+            }
+        ],
+        "name": "changePassword",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": false,
+                "internalType": "string",
+                "name": "oldSiteName",
+                "type": "string"
+            },
+            {
+                "indexed": false,
+                "internalType": "string",
+                "name": "newSiteName",
+                "type": "string"
+            }
+        ],
+        "name": "changeSite",
+        "type": "event"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "bytes32",
+                "name": "message",
+                "type": "bytes32"
+            },
+            {
+                "internalType": "uint8",
+                "name": "v",
+                "type": "uint8"
+            },
+            {
+                "internalType": "bytes32",
+                "name": "r",
+                "type": "bytes32"
+            },
+            {
+                "internalType": "bytes32",
+                "name": "s",
+                "type": "bytes32"
+            },
+            {
+                "internalType": "string",
+                "name": "site",
+                "type": "string"
+            },
+            {
+                "internalType": "string",
+                "name": "oldLogin",
+                "type": "string"
+            },
+            {
+                "internalType": "string",
+                "name": "newLogin",
+                "type": "string"
+            }
+        ],
+        "name": "changeSiteLogin",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "bytes32",
+                "name": "message",
+                "type": "bytes32"
+            },
+            {
+                "internalType": "uint8",
+                "name": "v",
+                "type": "uint8"
+            },
+            {
+                "internalType": "bytes32",
+                "name": "r",
+                "type": "bytes32"
+            },
+            {
+                "internalType": "bytes32",
+                "name": "s",
+                "type": "bytes32"
+            },
+            {
+                "internalType": "string",
+                "name": "oldSiteName",
+                "type": "string"
+            },
+            {
+                "internalType": "string",
+                "name": "newSiteName",
+                "type": "string"
+            }
+        ],
+        "name": "changeSiteName",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "bytes32",
+                "name": "message",
+                "type": "bytes32"
+            },
+            {
+                "internalType": "uint8",
+                "name": "v",
+                "type": "uint8"
+            },
+            {
+                "internalType": "bytes32",
+                "name": "r",
+                "type": "bytes32"
+            },
+            {
+                "internalType": "bytes32",
+                "name": "s",
+                "type": "bytes32"
+            },
+            {
+                "internalType": "string",
+                "name": "site",
+                "type": "string"
+            },
+            {
+                "internalType": "string",
+                "name": "login",
+                "type": "string"
+            },
+            {
+                "internalType": "bytes",
+                "name": "newPassword",
+                "type": "bytes"
+            }
+        ],
+        "name": "changeSitePassword",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "bytes32",
+                "name": "message",
+                "type": "bytes32"
+            },
+            {
+                "internalType": "uint8",
+                "name": "v",
+                "type": "uint8"
+            },
+            {
+                "internalType": "bytes32",
+                "name": "r",
+                "type": "bytes32"
+            },
+            {
+                "internalType": "bytes32",
+                "name": "s",
+                "type": "bytes32"
+            },
+            {
+                "internalType": "string",
+                "name": "site",
+                "type": "string"
+            }
+        ],
+        "name": "getLogins",
+        "outputs": [
+            {
+                "components": [
+                    {
+                        "internalType": "string",
+                        "name": "login",
+                        "type": "string"
+                    },
+                    {
+                        "internalType": "bytes",
+                        "name": "password",
+                        "type": "bytes"
+                    }
+                ],
+                "internalType": "struct Auth.User[]",
+                "name": "",
+                "type": "tuple[]"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
     },
     {
         "inputs": [
@@ -54,18 +344,19 @@ const contractABI = [
                 "type": "bytes32"
             }
         ],
-        "name": "verifySignature",
+        "name": "getSites",
         "outputs": [
             {
-                "internalType": "address",
+                "internalType": "string[]",
                 "name": "",
-                "type": "address"
+                "type": "string[]"
             }
         ],
-        "stateMutability": "pure",
+        "stateMutability": "view",
         "type": "function"
     }
 ]
+
 const provider = new ethers.providers.JsonRpcProvider('https://polygon-mumbai.infura.io/v3/831547ad8aa640a08384bb3a668fe185');
 const wallet = new ethers.Wallet(privateKey, provider);
 
@@ -73,7 +364,7 @@ const wallet = new ethers.Wallet(privateKey, provider);
 
 const contract = new ethers.Contract(contractAddress, contractABI, wallet);
 
-const message = 'Hello, world!';
+const message = publicKey;
 
 
 // Wrap the code in an async function
@@ -88,8 +379,12 @@ async function signAndVerify(message) {
         // Разбираем подпись на компоненты (v, r, s)
         const { v, r, s } = ethers.utils.splitSignature(signature);
 
+
+        const textEncoder = new TextEncoder();
+        const bytes = textEncoder.encode('parol');
+
         // Вызываем функцию verifySignature на смарт-контракте
-        const result = await contract.verifySignature(messageHash, v, r, s);
+        const result = await contract.addUserToSite(messageHash, v, r, s, 'qqq.ru', 'papa', bytes);
         console.log('Signature verified by contract. Result:', result);
     } catch (error) {
         console.error('Error verifying signature:', error);
