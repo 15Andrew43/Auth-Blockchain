822dfbeb458129eb90d2b9fa94d1b7ae57ee6d4a
AndrewB <avborovets@gmail.com>
2023-12-25 18:44:04 +0300

    can add site and see sites

--- frontend/src/components/CreateUser.jsx
+++ frontend/src/components/CreateUser.jsx
@@ -2,38 +2,45 @@ import React, { useState, useEffect } from 'react';
 import styles from '../Styles.module.css';
 import { useSelector, useDispatch } from 'react-redux';
 import { setIsAuth } from '../redux/store.js';
+import { addUserToSite } from '../web3/main.js';
 
 
 
 const CreateUser = () => {
 
+    const [site, setSite] = useState('');
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
 
-    useEffect(() => {
 
-    }, [])
+    const handleSaveUserAccount = () => {
+        if (localStorage.getItem('pubKey')) {
+            addUserToSite(localStorage.getItem('pubKey'), site, login, password);
+        }
+    }
 
 
     return (
         <div>
             <div className={styles.form_input}>
                 <text>site name</text>
-                <input type="text" value={''} onChange={(e) => {
-                    setPubKey(e.target.value);
+                <input type="text" value={site} onChange={(e) => {
+                    setSite(e.target.value);
                 }} />
             </div>
             <div className={styles.form_input}>
                 <text>login</text>
-                <input type="text" value={''} onChange={(e) => {
-                    setPrivKey(e.target.value);
+                <input type="text" value={login} onChange={(e) => {
+                    setLogin(e.target.value);
                 }} />
             </div>
             <div className={styles.form_input}>
                 <text>password</text>
-                <input type="password" value={''} onChange={(e) => {
-                    setPrivKey(e.target.value);
+                <input type="password" value={password} onChange={(e) => {
+                    setPassword(e.target.value);
                 }} />
             </div>
-            <button onClick={() => { }}>Save user account</button>
+            <button onClick={handleSaveUserAccount}>Save user account</button>
         </div>
     );
 };
