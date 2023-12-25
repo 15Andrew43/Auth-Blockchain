376369b6c7b8624096b4b608f41927b52463c3fa
AndrewB <avborovets@gmail.com>
2023-12-25 18:32:26 +0300

    can fetch data

--- frontend/src/components/Sites.jsx
+++ frontend/src/components/Sites.jsx
@@ -2,16 +2,28 @@ import React, { useEffect } from 'react';
 import styles from '../Styles.module.css';
 import { useSelector, useDispatch } from 'react-redux';
 import { setSites } from '../redux/store.js';
+import { getSites } from '../web3/main.js';
 
 const Sites = () => {
     const dispatch = useDispatch();
     const sites = useSelector(state => state.app.sites);
+    const isAuth = useSelector(state => state.app.is_auth);
 
-    useEffect(() => {
-        const fetchedSites = ['aaa', 'bbb', 'ccc'];
+    const fetchData = async () => {
+        const fetchedSites = await getSites(localStorage.getItem('pubKey'));
+        // const fetchedSites = ['Q', 'W', 'E'];
         console.log('SITES = ', fetchedSites);
         dispatch(setSites(fetchedSites));
-    }, [dispatch])
+    };
+
+    useEffect(() => {
+        console.log("QQQQQQQ", sites);
+        console.log(getSites);
+        if (localStorage.getItem('pubKey')) {
+            fetchData();
+        }
+        console.log("WWWWWWW", sites);
+    }, [dispatch]);
 
     return (
         <div className={styles.sites}>
